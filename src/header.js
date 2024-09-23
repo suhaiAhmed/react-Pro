7813dd378774064d485de664777c24d27d29c8e1
Suhail Ahmed <suhaillakho3@gmail.com>
2024-09-23 12:50:43 +0500

    first commit

---
+++ src/header.js
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return(
+        <>
+        <h1>Hello World !</h1>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/main.js
@@ -0,0 +1,11 @@
+const HeroSection = ()=>{
+    return( 
+        <>
+        <h3>
+            This is Main Section
+        </h3>
+        </>
+    );
+}
+
+export default HeroSection;
